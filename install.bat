b4965e50fd7110f8b97b046c29105ffb6d32c2e9
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-11-24 17:53:58 -0700

    Updated install script to ignore existing links

--- install.bat
+++ install.bat
@@ -18,18 +18,26 @@ set dotfiles=bashrc bash_profile bash dir_colors inputrc minttyrc gitconfig
 for %%A in (%dotfiles%) DO (
   set target=%dotfilesdir%\%%A
   set link=%USERPROFILE%\.%%A
+  set linkExists="0"
 
   if exist "!link!" (
-    if not exist "%backupdir%" (
-      mkdir "%backupdir%""
+    set Z=&& for %%B in ("!link!") do set Z=%%~aB
+    if "!Z:~8,1!" == "l" (
+      set linkExists="1"
+    ) else (
+      if not exist "%backupdir%" (
+        mkdir "%backupdir%""
+      )
+      echo Moving existing !link! to %backupdir%
+      move "!link!" "%backupdir%"
     )
-    echo Moving existing !link! to %backupdir%
-    move "!link!" "%backupdir%"
   )
-
-  if exist "!target!\" (
-    mklink /d "!link!" "!target!"
-  ) else (
-    mklink "!link!" "!target!"
+  
+  if !linkExists! == "0" (
+    if exist "!target!\" (
+      mklink /d "!link!" "!target!"
+    ) else (
+      mklink "!link!" "!target!"
+    )
   )
 )
