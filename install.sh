27b679a6e258f50b5468412c9384d7171c9be940
Arlo O'Keeffe <mailtoarlo@gmail.com>
2013-02-17 13:36:06 +0100

    Added sublime install to install script

--- install.sh
+++ install.sh
@@ -12,12 +12,10 @@ function fail()
 function createSymlinks()
 {
   local sourceDirectory=${1}
-  shift
-  local targetDirectory=${1}
-  shift
-  local backupDirectory=${1}
-  shift
-  local symlinks=(${@})
+  local targetDirectory=${2}
+  local backupDirectory=${3}
+  local withDot=${4}
+  declare -a symlinks=("${!5}")
 
   if [ ! -d "${sourceDirectory}" ]; then
     fail "Directory ${sourceDirectory} does not exist."
@@ -29,7 +27,11 @@ function createSymlinks()
 
   for symlink in "${symlinks[@]}"; do
 
-    local fullpath="${targetDirectory}/.${symlink}"
+    if [ ${withDot} -eq 1 ]; then
+      local fullpath="${targetDirectory}/.${symlink}"
+    else
+      local fullpath="${targetDirectory}/${symlink}"
+    fi
 
     # continue if already a symlink
     if [ -h "${fullpath}" ]; then
@@ -55,12 +57,37 @@ function createSymlinks()
   unset symlink
 }
 
-scriptpath=$( cd "$( dirname "$0" )" && pwd )
+function setupHome()
+{
+  local scriptPath=$( cd "$( dirname "$0" )" && pwd )
 
-# backup directory
-backupDir="${HOME}/dotfiles_backup"
+  # backup directory
+  local backupDir="${HOME}/dotfiles_backup"
 
-# list of files/folders to symlink in homedir
-dotfiles=("bashrc" "bash_profile" "bash" "dir_colors" "inputrc" "minttyrc" "gitconfig")
+  # list of files/folders to symlink in homedir
+  local dotfiles=("bashrc" "bash_profile" "bash" "dir_colors" "inputrc" "minttyrc" "gitconfig")
 
-createSymlinks "${scriptpath}/sublime" "${HOME}" "${backupDir}" "${dotfiles[@]}"
+  # setup symlinks in homedir
+  createSymlinks "${scriptPath}" "${HOME}" "${backupDir}" 1 dotfiles[@]
+}
+
+function setupSublime()
+{
+  local scriptPath=$( cd "$( dirname "$0" )" && pwd )
+
+  # the folders to symlink for Sublime
+  local sublimeFolders=("Installed Packages" "Packages" "Pristine Packages")
+
+  local sublimeDir="${HOME}/Library/Application Support/Sublime Text 2"
+
+  # backup directory
+  local backupDir="${HOME}/Library/Application Support/Sublime Text 2/Backup"
+
+  if [ -d "${sublimeDir}" ]; then
+    # setup symlinks for sublime
+    createSymlinks "${scriptPath}/sublime" "${sublimeDir}" "${backupDir}" 0 sublimeFolders[@]
+  fi
+}
+
+setupHome
+setupSublime
