c640f198a8f3a7c86d9fd2233c937916a4cda0e3
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2014-10-16 13:53:52 +0200

    Set proxy authentication defaults

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,3 +21,5 @@ Set-Alias open Invoke-Item
 if (Test-Path "C:\Program Files\Sublime Text 3") {
     Set-Alias edit "C:\Program Files\Sublime Text 3\sublime_text"
 }
+
+[net.webrequest]::defaultwebproxy.credentials = [net.credentialcache]::defaultcredentials
