31f9c35bcd1632f52af484acfea5d328d6404ba9
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2014-10-16 13:45:22 +0200

    Add default configuration for powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,23 @@
+chcp 65001 | out-null
+
+function prompt {
+    '[' + $env:username + '@' + $env:computername + ' ' + $pwd + "]`r`n$ "
+}
+
+function .. {
+    Set-Location ..
+}
+function ... {
+    Set-Location ..\..
+}
+function .... {
+    Set-Location ..\..\..
+}
+
+Set-Alias l Get-ChildItem
 Set-Alias ll Get-ChildItem
+Set-Alias open Invoke-Item
+
+if (Test-Path "C:\Program Files\Sublime Text 3") {
+    Set-Alias edit "C:\Program Files\Sublime Text 3\sublime_text"
+}
