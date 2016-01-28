195fe4557d0c7f0fd0406cb458c8514a8873314c
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-28 16:18:34 +0100

    Update stree.cmd

--- bin/stree.cmd
+++ bin/stree.cmd
@@ -1,4 +1,24 @@
 @echo off
+
+setlocal enabledelayedexpansion
+
 set directory=%~f1
-if %directory:~-1%==\ set directory=%directory:~0,-1%
-start "" "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe" -f %directory%
+if [%directory%] == [] set directory=%CD%
+
+if exist %directory%\NUL (
+    set directory=%directory%
+) else (
+    set directory=%~dp1
+)
+
+pushd %directory%
+
+for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set directory=%%i
+git rev-parse --show-toplevel 1> nul 2> nul
+
+if %errorlevel% EQU 0 (
+    start "" "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe" -f %directory%
+)
+
+popd
+endlocal
