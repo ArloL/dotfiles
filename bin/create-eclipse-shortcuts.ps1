dabc4880950e6ce13629286966bc447124e00704
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-12-22 10:33:17 +0100

    Use GetFolderPath("Startmenu")

--- bin/create-eclipse-shortcuts.ps1
+++ bin/create-eclipse-shortcuts.ps1
@@ -1,4 +1,6 @@
-New-Item -ItemType Directory -Force -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Eclipse\" | Out-Null
+$Startmenu = [Environment]::GetFolderPath("Startmenu")
+
+New-Item -ItemType Directory -Force -Path "$Startmenu\Programs\Eclipse\" | Out-Null
 
 $EclipseExes = gci -recurse -filter "eclipse.exe" -File
 
@@ -8,11 +10,11 @@ foreach ($EclipseExe in $EclipseExes) {
 
     $ProjectDirectory = $EclipseExe.Directory.Parent
 
-    $ShortcutFile = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Eclipse\$ProjectDirectory.lnk"
+    $ShortcutFile = "$Startmenu\Programs\Eclipse\$ProjectDirectory.lnk"
 
     $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
     $Shortcut.TargetPath = $EclipseExe.FullName
-    $Shortcut.WorkingDirectory = $EclipseExe.Directory.FullName 
+    $Shortcut.WorkingDirectory = $EclipseExe.Directory.FullName
     $Shortcut.Save()
 
 }
