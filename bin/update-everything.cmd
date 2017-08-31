c305b8ef85fe54b4a94cd8ca84eb69b1579993fe
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-08-31 14:45:54 +0200

    Add updating dotfiles

--- bin/update-everything.cmd
+++ bin/update-everything.cmd
@@ -1,4 +1,16 @@
 @echo off
 
+if exist "%HOME%/.dotfiles" (
+    cd %HOME%/.dotfiles
+    git pull --rebase
+    git push origin master
+)
+
+if exist "%HOME%/dotfiles" (
+    cd %HOME%/dotfiles
+    git pull --rebase
+    git push origin master
+)
+
 sudo choco upgrade all
 scoop update
