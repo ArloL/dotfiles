815c649679064eec970c1753a42d66a3f10d2255
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2018-09-01 10:01:53 +0200

    Add another key for grepWin to deactivate

--- context-menu-disable-grepWin.bat
+++ context-menu-disable-grepWin.bat
@@ -10,3 +10,11 @@ reg add "%registryRoot%\Directory\background\shell\%key%" /v LegacyDisable /t RE
 reg add "%registryRoot%\Directory\shell\%key%" /v LegacyDisable /t REG_SZ /f
 reg add "%registryRoot%\Drive\shell\%key%" /v LegacyDisable /t REG_SZ /f
 reg add "%registryRoot%\Folder\shell\%key%" /v LegacyDisable /t REG_SZ /f
+
+set key=grepWin
+
+reg add "%registryRoot%\*\shell\%key%" /v LegacyDisable /t REG_SZ /f
+reg add "%registryRoot%\Directory\background\shell\%key%" /v LegacyDisable /t REG_SZ /f
+reg add "%registryRoot%\Directory\shell\%key%" /v LegacyDisable /t REG_SZ /f
+reg add "%registryRoot%\Drive\shell\%key%" /v LegacyDisable /t REG_SZ /f
+reg add "%registryRoot%\Folder\shell\%key%" /v LegacyDisable /t REG_SZ /f
