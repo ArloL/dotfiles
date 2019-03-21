67572951e1b4498415e6a7b5703ecf2cb283c3c0
Arlo O'Keeffe <arlo.okeeffe@dw.com>
2019-03-21 15:50:04 +0100

    Disable tortoise svn on more types

--- context-menu-disable/context-menu-disable-tortoise-svn.bat
+++ context-menu-disable/context-menu-disable-tortoise-svn.bat
@@ -10,3 +10,7 @@ reg add "%registryRoot%\Directory\Background\shellex\ContextMenuHandlers\%key%" 
 reg add "%registryRoot%\Directory\shellex\ContextMenuHandlers\%key%" /d "---" /f
 reg add "%registryRoot%\Drive\shellex\ContextMenuHandlers\%key%" /d "---" /f
 reg add "%registryRoot%\Folder\shellex\ContextMenuHandlers\%key%" /d "---" /f
+reg add "%registryRoot%\InternetShortcut\shellex\ContextMenuHandlers\%key%" /d "---" /f
+reg add "%registryRoot%\LibraryFolder\background\shellex\ContextMenuHandlers\%key%" /d "---" /f
+reg add "%registryRoot%\LibraryLocation\shellex\ContextMenuHandlers\%key%" /d "---" /f
+reg add "%registryRoot%\lnkfile\shellex\ContextMenuHandlers\%key%" /d "---" /f
