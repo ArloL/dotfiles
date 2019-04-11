cbd72bd2ab2ed3a57bd1a15953bcb4fa291bcd4a
Arlo O'Keeffe <arlo.okeeffe@dw.com>
2019-04-11 15:23:01 +0200

    Disable officescan context menu on drives

--- context-menu-disable/context-menu-disable-officescan.bat
+++ context-menu-disable/context-menu-disable-officescan.bat
@@ -6,3 +6,4 @@ set registryRoot=HKCU\Software\Classes
 set key=OfficeScan NT
 
 reg add "%registryRoot%\*\shellex\ContextMenuHandlers\%key%" /d "---" /f
+reg add "%registryRoot%\Drive\shellex\ContextMenuHandlers\%key%" /d "---" /f
