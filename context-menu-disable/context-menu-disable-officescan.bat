308f38432906887efe34c7bad23792fc027859c5
Arlo O'Keeffe <arlo.okeeffe@dw.com>
2019-04-18 09:51:36 +0200

    Disable officescan context menu on directories

--- context-menu-disable/context-menu-disable-officescan.bat
+++ context-menu-disable/context-menu-disable-officescan.bat
@@ -7,3 +7,4 @@ set key=OfficeScan NT
 
 reg add "%registryRoot%\*\shellex\ContextMenuHandlers\%key%" /d "---" /f
 reg add "%registryRoot%\Drive\shellex\ContextMenuHandlers\%key%" /d "---" /f
+reg add "%registryRoot%\Directory\shellex\ContextMenuHandlers\%key%" /d "---" /f
