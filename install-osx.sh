85e284613747f99eb66fefb5468b3bc420fde3b2
Arlo O'Keeffe <tiger@k5d.de>
2020-07-06 20:49:49 +0200

    Remove internet-weg script

--- install-osx.sh
+++ install-osx.sh
@@ -84,7 +84,7 @@ setupBin() {
     local backupDir="${HOME}/dotfiles_backup/bin"
 
     # list of files/folders to symlink in homedir
-    local binfiles=("update-everything" "internet-weg")
+    local binfiles=("update-everything")
 
     mkdir -p "${HOME}/bin"
 
