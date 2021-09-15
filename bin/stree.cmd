77ba3e916663eb12114754d7b479598b9d650ac3
Art Gola <artgola@hotmail.com>
2021-09-14 23:57:46 -0400

    resolved path issues
    Source tree installs into %localappdata% now.
    Also fixed issues with how the directory was handled when it contained spaces.

--- bin/stree.cmd
+++ bin/stree.cmd
@@ -1,11 +1,23 @@
 @echo off
-
 setlocal enabledelayedexpansion
 
-set directory=%~f1
-if [%directory%] == [] set directory=%CD%
+set sourcetreepath=SourceTree\SourceTree.exe
+if exist "%programfiles%\Atlassian\%sourcetreepath%" (
+    set sourcetreepath=%programfiles%\Atlassian\%sourcetreepath%
+) else if exist "%programfiles(x86)%\Atlassian\%sourcetreepath%" (
+    set "sourcetreepath=%programfiles(x86)%\Atlassian\%sourcetreepath%"
+) else if exist "%localappdata%\%sourcetreepath%" (
+    set sourcetreepath=%localappdata%\%sourcetreepath%
+) else (
+    echo [Error]: Could not find SourceTree.exe - is it installed? 1>&2
+    goto :eof
+) 
 
-if exist %directory%\NUL (
+set directory=%~f1
+if "%directory%" == "" set directory=%CD%
+echo %directory%
+
+if exist "%directory%\NUL" (
     set directory=%directory%
 ) else (
     set directory=%~dp1
@@ -17,7 +29,7 @@ for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set directory=%%i
 git rev-parse --show-toplevel 1> nul 2> nul
 
 if %errorlevel% EQU 0 (
-    start "" "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe" -f %directory:/=\%
+    start "" "%sourcetreepath%" -f "%directory:/=\%" log
 )
 
 popd
