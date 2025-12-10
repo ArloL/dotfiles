16386d07d310e24ac755d2b55ba3cb2eb1246cce
Arlo O'Keeffe <tiger@k5d.de>
2025-12-10 14:53:27 +0100

    set dotfiles

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+OS="$(uname)"
+if [ "${OS}" = "Linux" ]; then
+  	platform=linux
+elif [ "${OS}" = "Darwin" ]; then
+  	platform=macos
+fi
+
+sh "$(dirname "$0")/install-${platform}.sh"
