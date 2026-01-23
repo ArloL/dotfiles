042c1393fa08f4c1ca61d44e8bde0a53bd2acc3e
Arlo O'Keeffe <tiger@k5d.de>
2026-01-23 10:03:05 +0100

    add get-audio

--- install-macos.sh
+++ install-macos.sh
@@ -84,7 +84,7 @@ setupBin() {
     local backupDir="${HOME}/dotfiles_backup/bin"
 
     # list of files/folders to symlink in homedir
-    local binfiles=("update-everything" "base64-encode-stdin.sh" "yarn-link"  "git-push-main")
+    local binfiles=("update-everything" "base64-encode-stdin.sh" "yarn-link"  "git-push-main" "get-audio")
 
     mkdir -p "${HOME}/bin"
     chflags hidden "${HOME}/bin"
